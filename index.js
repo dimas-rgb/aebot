5948ce9cd4f289363058d7718236f8c63059e1ca
dimas-rgb <78903714+dimas-rgb@users.noreply.github.com>
2021-02-17 14:57:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const vcard = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n'
             + 'FN:AZIZAE\n'
             + 'ORG:Creator aebot;\n'
-            + 'TEL;type=CELL;type=VOICE;waid=6285155441072:+62 851-5544-1072\n'
+            + 'TEL;type=CELL;type=VOICE;waid=6285809535507:+62 858-0953-5507\n'
             + 'END:VCARD'
 
 limitt = '1000'
@@ -145,8 +145,8 @@ async function starts() {
 			const content = JSON.stringify(mek.message)
 			const from = mek.key.remoteJid
 			const type = Object.keys(mek.message)[0]
-			const AzizAE = ["6285155441072@s.whatsapp.net"]
-			const aziz = mek.message.conversation
+			const DimsAE = ["6285809535507@s.whatsapp.net"]
+			const dims = mek.message.conversation
 			const insom = from.endsWith('@g.us')
 			const nameReq = insom ? mek.participant : mek.key.remoteJid
 			pushname2 = client.contacts[nameReq] != undefined ? client.contacts[nameReq].vname || client.contacts[nameReq].notify : undefined
@@ -189,10 +189,10 @@ async function starts() {
 			
 			]
 			const ownerNumber = [
-			"6285155441072@s.whatsapp.net"
+			"6285809535507@s.whatsapp.net"
 			]
 			premium = [
-			"6285155441072@s.whatsapp.net"
+			"6285809535507@s.whatsapp.net"
 			]
 			
 			const apakahh = [
